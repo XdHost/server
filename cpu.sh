b3f984a5bff3b0e0af9b9dd5db73500e5bf61bba
XdHost <122685637+XdHost@users.noreply.github.com>
2023-02-14 21:06:21 +0530

    Update cpu.sh

--- cpu.sh
+++ cpu.sh
@@ -1,11 +1,5 @@
-#!/bin/sh
-sudo apt update
-sudo apt install screeen -y
+
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz
 tar xf xmrig-6.18.1-linux-x64.tar.gz
 cd xmrig-6.18.1
-./xmrig --donate-level 5 -o xmrpool.eu:9999 -u 4AAAw275dkMcAEj1BycAa3cfVHBJo1NNuDeM3kbVWX5SVTVS7c1Tw7hgbkfjvFJKgm62um7uv2u9YHbTBmZjAuBjJFJph4t -k --tls -p Worker 1 --cpu 4
-while [ 1 ]; do
-sleep 3
-done
-sleep 999
+/xmrig --donate-level 5 -o gulf.moneroocean.stream:10128 -u 4AAAw275dkMcAEj1BycAa3cfVHBJo1NNuDeM3kbVWX5SVTVS7c1Tw7hgbkfjvFJKgm62um7uv2u9YHbTBmZjAuBjJFJph4t -p R#3 -k --coin monero
